c250b995bc5a828986cb5bd8be60b97e6e6267fa
satinder <satindersingh472@gmail.com>
2022-10-11 19:21:54 -0600

    finished

--- app.py
+++ app.py
@@ -2,8 +2,35 @@
 from dbhelpers import conn_exe_close
 from apihelpers import get_display_results, verify_endpoints_info
 from flask import Flask, request, make_response
+import dbcreds
 import json
 
 app = Flask(__name__)
 
+@app.post('/api/painting')
+def add_painting():
+    invalid = verify_endpoints_info(request.json,['artist','name','image_url'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid,default=str),400)
+    results_json = get_display_results('call add_painting(?,?,?)',
+    [request.json.get('artist'),request.json.get('name'),request.json.get('image_url')])
+    return results_json
+
+@app.get('/api/painting')
+def all_paintings():
+    invalid = verify_endpoints_info(request.args,['artist'])
+    if(invalid != None):
+        return make_response(json.dumps(invalid,default=str),400)
+    results_json = get_display_results('call all_paintings(?)',[request.args.get('artist')])
+    return results_json
+
+
+if(dbcreds.production_mode == True):
+    print('Running in production mode')
+    app.run(debug=True)
+else:
+    from flask_cors import CORS
+    CORS(app)
+    print('Running in Testing mode')
+    app.run(debug=True)
 app.run(debug=True)
---
+++ backup_file.sql
@@ -0,0 +1,102 @@
+-- MariaDB dump 10.19  Distrib 10.6.7-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: flask_modes
+-- ------------------------------------------------------
+-- Server version	10.6.7-MariaDB-2ubuntu1.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `painting`
+--
+
+DROP TABLE IF EXISTS `painting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `painting` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `artist` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `date_painted` date DEFAULT current_timestamp(),
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `image_url` mediumtext COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `painting`
+--
+
+LOCK TABLES `painting` WRITE;
+/*!40000 ALTER TABLE `painting` DISABLE KEYS */;
+INSERT INTO `painting` VALUES (1,'painter_one','2022-10-11','artist_one','https://images.unsplash.com/photo-1665509862441-4712c137e8a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'),(2,'satinder','2022-10-11','painting_amazing','https://images.unsplash.com/photo-1665509862441-4712c137e8a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60');
+/*!40000 ALTER TABLE `painting` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'flask_modes'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `add_painting` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_painting`(artist_input varchar(100), name_input varchar(100), image_url_input mediumtext)
+    MODIFIES SQL DATA
+BEGIN
+	insert into painting(artist,name,image_url)
+	values(artist_input, name_input, image_url_input);
+	SELECT LAST_INSERT_ID(); 
+	commit;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `all_paintings` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `all_paintings`(artist_input varchar(100))
+BEGIN
+	SELECT p.id,p.name,p.date_painted,p.image_url 
+	from painting p
+	where p.artist = artist_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-11 19:21:35
